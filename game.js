f251e2fd6b28c3a899b9de012ad3e9e638ef1002
Filippa <filippa.piazolo@gmail.com>
2022-05-23 14:48:05 +0200

    tried moebius transformation, does not work

--- game.js
+++ game.js
@@ -12,15 +12,19 @@ var level = 1;
 
 function setup() {
     createCanvas( w, h );
-    ship = new Ship(w/2,h/2, 10);
+    /*ship = new Ship(w/2,h/2, 10);
     for (let i = 0; i < 5; i++) {       
         asteroids.push(new Asteroid());
-    }
+    }*/
 }
 
 function draw() {
     background( 240 );
-    
+
+    translate(10,h/2);
+    drawTransformation();
+
+    /*
     for(var i=0; i<asteroids.length;i++){
         asteroids[i].render();
         asteroids[i].move();
@@ -44,7 +48,52 @@ function draw() {
     ship.show();
     ship.turn();
     ship.move();
-    ship.edge();
+    ship.edge();*/
+}
+
+function drawTransformation()
+{   
+    var C = createVector(200,0);
+    var R = 100;
+    var pos1 = createVector(0,10);
+    var pos2 = createVector(0,30);
+    var pos3 = createVector(0,50);
+    var pos4 = createVector(0,70);
+    var pos5 = createVector(0,90);
+    var pos6 = createVector(0,110);
+    var pos7 = createVector(0,130);
+    var pos8 = createVector(0,150);
+    stroke(0, 255, 0);
+    strokeWeight(4);
+
+    point(pos1.x, pos1.y);
+    point(pos2.x, pos2.y);
+    point(pos3.x, pos3.y);
+    point(pos4.x, pos4.y);
+    point(pos5.x, pos5.y);
+    point(pos6.x, pos6.y);
+    point(pos7.x, pos7.y);
+    point(pos8.x, pos8.y);
+
+    point(C.x,C.y);
+    //circle(C.x, C.y, R);
+
+    var newpos1 = moebtransform(pos1,C,R);
+    var newpos2 = moebtransform(pos2,C,R);
+    var newpos3 = moebtransform(pos3,C,R);
+    var newpos4 = moebtransform(pos4,C,R);
+    var newpos5 = moebtransform(pos5,C,R);
+    var newpos6 = moebtransform(pos6,C,R);
+    var newpos7 = moebtransform(pos7,C,R);
+    var newpos8 = moebtransform(pos8,C,R);
+    point(newpos1.x, newpos1.y);
+    point(newpos2.x, newpos2.y);
+    point(newpos3.x, newpos3.y);
+    point(newpos4.x, newpos4.y);
+    point(newpos5.x, newpos5.y);
+    point(newpos6.x, newpos6.y);
+    point(newpos7.x, newpos7.y);
+    point(newpos8.x, newpos8.y);
 }
 
 
