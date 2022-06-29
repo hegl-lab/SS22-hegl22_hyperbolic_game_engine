fa770cb572202e5d53f27df405ea6eaa41118c6e
Filippa <filippa.piazolo@gmail.com>
2022-06-29 15:47:03 +0200

    change radius, and scale; problem with geodesic

--- game.js
+++ game.js
@@ -11,18 +11,32 @@ let poincareDisk, m, geodesic;
 
 function setup() {
     createCanvas( w, w );
-    poincareDisk = new Circle(0,0,300);
-    m = new Point(0,0);
+    poincareDisk = new Circle(0,0,1);
+    //m = new Point(0,0);
 
     for(let i = 0; i<1; i++){
-        var x = random(-150,150);
-        var y = random(-150,150);
-        var v1 = random(-10,10);
-        var v2 = random(-10,10);
+        var x = random(-1,1);
+        var y = random(-1,1);
+        var v1 = random(-1,1);
+        var v2 = random(-1,1);
         var speed = random(-10,10);
         if (speed == 0)
             speed += 1;
-        asteroids.push(new PointMovingOnGeodesic(x,y,v1,v2,7,poincareDisk,speed));
+        asteroids.push(new PointMovingOnGeodesic(x,y,v1,v2,0.03,poincareDisk,speed));
+        /*asteroids.push(new PointMovingOnGeodesic(0,0.2,v1,v2,0.03,poincareDisk,speed));
+        asteroids.push(new PointMovingOnGeodesic(0,0.4,v1,v2,0.03,poincareDisk,speed));
+        asteroids.push(new PointMovingOnGeodesic(0,0.6,v1,v2,0.03,poincareDisk,speed));
+        asteroids.push(new PointMovingOnGeodesic(0,0.8,v1,v2,0.03,poincareDisk,speed));
+        
+        asteroids.push(new PointMovingOnGeodesic(-0.2,0,v1,v2,0.03,poincareDisk,speed));
+        asteroids.push(new PointMovingOnGeodesic(-0.4,0,v1,v2,0.03,poincareDisk,speed));
+        asteroids.push(new PointMovingOnGeodesic(-0.6,0,v1,v2,0.03,poincareDisk,speed));
+        asteroids.push(new PointMovingOnGeodesic(-0.8,0,v1,v2,0.03,poincareDisk,speed));
+
+        asteroids.push(new PointMovingOnGeodesic(0.2,0,v1,v2,0.05,poincareDisk,speed));
+        asteroids.push(new PointMovingOnGeodesic(0.4,0,v1,v2,0.05,poincareDisk,speed));
+        asteroids.push(new PointMovingOnGeodesic(0.6,0,v1,v2,0.05,poincareDisk,speed));
+        asteroids.push(new PointMovingOnGeodesic(0.8,0,v1,v2,0.05,poincareDisk,speed));*/
     }
    
     ship = new Ship(-50,40,5);
@@ -35,9 +49,9 @@ function draw() {
     background( 240 );
 
     translate(w/2,w/2);
-
+    scale(1, -1);
     poincareDisk.show();
-    m.show();
+    //m.show();
     for(let i=0; i<asteroids.length; i++){
         asteroids[i].show();
         //asteroids[i].move();
@@ -66,12 +80,12 @@ function draw() {
     ship.turn();
     ship.move();
 
-    for(let i=0; i<asteroids.length; i++){
+    /*for(let i=0; i<asteroids.length; i++){
         var point1 = asteroids[i].pt;
         var collide = collisionDetection(point1.x,point1.y,point1.r, ship.pos.x, ship.pos.y, ship.radius);
         if (collide == true)
             console.log('collision')
-    }
+    }*/
 }
 
 
@@ -103,7 +117,8 @@ function hyperbolicDistance(x1,y1,x2,y2){ //p1 = (x1,y1); p2 = (x2,y2)
 }
 
 
-function collisionDetection(x1,y1,r1,x2,y2,r2){ //p1 = (x1,y1) with radius r1; p2 = (x2,y2) with radius r2
+/*function collisionDetection(x1,y1,r1,x2,y2,r2){ //p1 = (x1,y1) with radius r1; p2 = (x2,y2) with radius r2
+    var X=1;
     var distancePoints = hyperbolicDistance(x1,y1,x2,y2);
     var r1x = x1+r1;
     var r2x = x2+r2;
@@ -112,11 +127,8 @@ function collisionDetection(x1,y1,r1,x2,y2,r2){ //p1 = (x1,y1) with radius r1; p
     var hyperbolicRadius2 = hyperbolicDistance(x1,y2,r2x,y2);
     //PROBLEM: Visual difference between radius in hyperbolic space and on canvas for drawing of circle
     //change view of circles,... to the actual size in hyperbolic space
-    console.log(distancePoints);
-    console.log(hyperbolicRadius1);
-    console.log(hyperbolicRadius2);
     if (distancePoints < (hyperbolicRadius1 + hyperbolicRadius2))
-        return true
+        return true;
     else  
-        return false
-}
+        return false;
+}*/
--- geodesic.js
+++ geodesic.js
@@ -2,8 +2,11 @@
 class Geodesic {
     constructor(pt1, circle){
         this.pt1 = pt1;
+        console.log(this.pt1);
         this.circle = circle;
         this.pt2 = this.circle.reflectPoint(this.pt1);
+        
+        console.log(this.pt2);
         if(this.pt1.x == this.circle.x && this.pt1.y == this.circle.y){ //pt1 is midpoint of circle
             this.m = new LineSegment(this.pt1.x-300*this.pt1.dir.x,this.pt1.y-300*this.pt1.dir.y, (this.pt1.x+300*this.pt1.dir.x), (this.pt1.y + 300*this.pt1.dir.y))
  
@@ -16,6 +19,7 @@ class Geodesic {
     show(){
         push();
         this.m.show();
+        this.pt2.show();
         pop();
     }
 }
@@ -33,14 +37,16 @@ class PointMovingOnGeodesic {
     show() {
         
         push();
-        if (Math.sqrt(this.pt.x*this.pt.x + this.pt.y*this.pt.y) < this.circle.r) //only points in the poincare disc are shown
+        if (Math.sqrt(this.pt.x*this.pt.x + this.pt.y*this.pt.y) < 1){ //only points in the poincare disc are shown{
             this.pt.show();
-        //this.geodesic.show();
+        }
+            
+        this.geodesic.show();
         pop();
     }
 
     move() {
-        var del_alpha = ((sq(this.circle.r) - (sq(this.pt.x)+sq(this.pt.y)))*0.000015)/(2*this.geodesic.m.r);
+        var del_alpha = ((1 - (sq(this.pt.x)+sq(this.pt.y)))*0.000015)/(2*this.geodesic.m.r);
         this.alpha = this.alpha + del_alpha;
         var newX, newY;
         var newX1 = this.geodesic.m.r*Math.cos(this.alpha) + this.geodesic.m.x;
@@ -60,19 +66,47 @@ class PointMovingOnGeodesic {
         var newV2 = 0;
         
         //calculate new radius
-        var dist1 = sqrt(sq(this.pt.x)+sq(this.pt.y));
+        /*var dist1 = sqrt(sq(this.pt.x)+sq(this.pt.y));
         var dist2 = sqrt(sq(newX)+sq(newY));
         var del_radius = 0.00000015*(sq(poincareDisk.r) - (sq(newX)+sq(newY)));
         if (dist1<dist2)
             this.pt.r = this.pt.r - del_radius;
         else
-            this.pt.r = this.pt.r + del_radius;
+            this.pt.r = this.pt.r + del_radius;*/
         //var radius = this.pt.r * Math.sqrt(sq(Math.cos(this.alpha))+sq(Math.sin(this.alpha)))*0.999;
         //this.pt = new Point(newX, newY, newV1, newV2, radius);
         this.pt = new Point(newX, newY, newV1, newV2, this.pt.r);
     }
 }
 
+
+function arg(a) {
+    let mult = 0;
+    if(a[0] <0) {
+        mult = 1;
+    }
+    return atan(a[1]/a[0]) + mult * PI;
+}
+
+function sub2(a,b) {
+    return [a[0] - b[0], a[1] - b[1]];
+}
+
+function add2(a,b) {
+    return [a[0] + b[0], a[1] + b[1]];
+}
+
+function mog(a) {
+    return sq(a[0]) + sq(a[1]);
+}
+
+function car(a) {
+    return [a[0] * cos(a[1]), a[0] * sin(a[1])];
+}
+
+function mul(a,b) {
+    return [a[0] * b[0] - a[1] * b[1], a[0] * b[1] + a[1] * b[0]];
+}
 
 class Point {
     constructor( x, y, v1, v2, r) {
@@ -91,17 +125,24 @@ class Point {
     }
     
     show() {
+        let a = sqrt(mog([this.x,this.y]));
+        let angle = arg([this.x,this.y]);
+        let pm = ((1+a) * exp(this.r) - (1-a)) / ((1 + a) * exp(this.r) + (1 - a));
+        let p_koordinate = car([pm,angle]);
+        let qm = ((1+ a) - (1 - a)*exp(this.r)) / ((1 + a) + (1 - a) * exp(this.r));
+        let q_koordinate = car([qm,angle]);
+        let b = mul(add2(p_koordinate,q_koordinate), [0.5, 0]);
+        let l = 0.5 * sqrt(mog(sub2(p_koordinate,q_koordinate)));
+
         push();
         stroke( 0, 0, 255 );
         fill( 0, 0, 255 );
-        //var radius = this.calculateRadius();
-        circle(this.x, this.y, this.r);
-        //this.dir.show();
+        circle(b[0] * w/2, b[1] * w/2, w * l);
         pop();
     }
 
 
-    createCircleDir(pt1){ //create circle from two given points and direction
+    createCircleDir(pt1){ //create circle from two given points and direction (geodesic)
         let dirx = this.dir.x;
         let diry = this.dir.y;
         let x1 = pt1.x;
@@ -120,7 +161,9 @@ class Point {
             m1 = this.x + radius*diry;
             m2 = this.y - radius*dirx;
         }
-        let c = new Circle(m1, m2, radius);
+        console.log(m1,m2);
+        console.log(radius);
+        let c = new Circle(m1, m2, radius); //Kreis wird noch nicht an richtige Stelle gezeichnet...
         return c;
     }
 }
@@ -137,7 +180,7 @@ class Circle {
         push();
         stroke(0);
         noFill();
-        circle(this.x,this.y,this.r*2);
+        circle(this.x,this.y,this.r*w);
         pop();
     }
 
@@ -145,10 +188,11 @@ class Circle {
         let x0 = pt0.x;
         let y0 = pt0.y;
         let len1 = dist(this.x, this.y, x0, y0);
-        let len2 = (this.r*this.r)/len1;
+        let len2 = 1/len1;
         let x1 = this.x + len2*(x0-this.x)/len1;
         let y1 = this.y + len2*(y0-this.y)/len1;
         let pt = new Point(x1,y1, pt0.r);
+        console.log(pt);
         return pt;
     }
 }
